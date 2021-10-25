7f18b5e13403352b2a73dc24fb72521eb083bdb6
JK <kuku6471@gmail.com>
2021-10-25 20:00:43 +0900

    modify auth data to use database data

--- server/data/auth.js
+++ server/data/auth.js
@@ -1,56 +1,26 @@
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
-
-//password: abcd1234, hashed: $2b$10$CDdC9ioSc/oLCYQBGhqR5ee9.pG8fgFZBAsChbvLHkYcp2xwGNafq
-
-let users = [
-  {
-    id: "1",
-    username: "JK",
-    password: "$2b$10$CDdC9ioSc/oLCYQBGhqR5ee9.pG8fgFZBAsChbvLHkYcp2xwGNafq",
-    name: "JK",
-    email: "kuku6471@naver.com",
-    url: "https://res.cloudinary.com/dpvhkp8oq/image/upload/v1632646994/Motion/moxvxyhmceuumjye3lth.jpg",
-  },
-  {
-    id: "2",
-    username: "SONG",
-    password: "$2b$10$CDdC9ioSc/oLCYQBGhqR5ee9.pG8fgFZBAsChbvLHkYcp2xwGNafq",
-    name: "songsong",
-    email: "kuku6471@naver.com",
-    url: "https://res.cloudinary.com/dpvhkp8oq/image/upload/v1632646994/Motion/moxvxyhmceuumjye3lth.jpg",
-  },
-];
-
-const secretKey = "KMduSUkyCVrT0WWWE!rlqzH2rOxK^li8";
+import { db } from "../db/database.js";
 
 export async function findByUsername(username) {
-  return users.find((user) => user.username === username);
+  return db
+    .execute("SELECT * FROM users WHERE username=?", [username]) //
+    .then((result) => result[0][0]);
 }
 
 export async function findById(id) {
-  return users.find((user) => user.id === id);
+  return db
+    .execute("SELECT * FROM users WHERE id=?", [id]) //
+    .then((result) => result[0][0]);
 }
 
 export async function create({ username, password, name, email, url }) {
-  const user = {
-    id: Date.now().toString(),
-    username,
-    password,
-    name,
-    email,
-    url,
-  };
-  users.push(user);
-  return user.id;
-}
-
-export async function check(token) {
-  const username = jwt.verify(token, secretKey, (error, decoded) => {
-    if (error) {
-      throw new Error(error);
-    }
-    return decoded.username;
-  });
-  return { username, token };
+  return db
+    .execute(
+      "INSERT INTO users (username, password, name, email, url) VALUES (?,?,?,?,?)",
+      [username, password, name, email, url]
+    )
+    .then((result) => {
+      return result[0].insertId;
+    });
 }
